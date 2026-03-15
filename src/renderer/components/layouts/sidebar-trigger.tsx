e24b3f15008e7a05da42d265c355e0efa34c796e
Hyunsol Park <hpark0011@gmail.com>
2026-03-15 11:39:29 +0900

    refine sidebar trigger ui

--- src/renderer/components/layouts/sidebar-trigger.tsx
+++ src/renderer/components/layouts/sidebar-trigger.tsx
@@ -4,17 +4,12 @@ function SidebarTriggerVerticalLine({ isOpen }: { isOpen: boolean }) {
   return (
     <div
       className={cn(
-        // Positioning
         "relative",
-        // Sizing
         "w-[1.5px]",
-        // Shape
-        "rounded-full",
-        // Background
-        "bg-icon/70",
-        // Interactive states
         "transition-all duration-200",
-        isOpen ? "h-full left-[5px] bg-icon" : "h-[calc(100%-3px)] left-0.5",
+        isOpen
+          ? "h-full left-[5px] bg-icon rounded-none"
+          : "h-[calc(100%-3px)] left-0.5 rounded-full bg-icon/70",
       )}
     />
   );
@@ -24,11 +19,8 @@ export function SidebarTrigger({ isOpen }: { isOpen: boolean }) {
   return (
     <div
       className={[
-        // Layout & alignment
         "flex items-center",
-        // Sizing
         "w-[15px] h-3.5",
-        // Shape
         "border-icon border-[1.5px] rounded-[4px]",
       ].join(" ")}
     >
